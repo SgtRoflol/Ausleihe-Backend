2d31cb75a55176cfaeb75a280958e7a44b5c937b
mnjixx <kayleemaria2@gmail.com>
2024-06-02 16:33:48 +0200

    update equipment

--- routes/equipment.js
+++ routes/equipment.js
@@ -4,5 +4,8 @@ var router = express.Router();
 //import controller functions
 var controller = require('../controllers/equipmentController');
 
-
+router.route('/')
+.get((res,req,next)=>{
+    let response = controller.getAll;
+})
 module.exports = router;
