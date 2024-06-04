c2715661b6f832bea5593fdc7d8609fd43bd70ac
mnjixx <kayleemaria2@gmail.com>
2024-06-04 16:48:53 +0200

    userIdMussBestehen

--- controllers/equipmentController.js
+++ controllers/equipmentController.js
@@ -1,3 +1,6 @@
+/*
+Code by: Kaylee Andres
+*/
 var express = require('express');
 var router = express.Router();
 
@@ -6,84 +9,104 @@ const fs = require('fs');
 var rawdata = fs.readFileSync('./models/equipments.json');
 var userdata = fs.readFileSync('./models/users.json');//maybe later
 
+//if there is no Object in equipments create an array, else JSON to js Objekt
 if (rawdata.length <= 0) {
-    var equipments = [];
-  } else {
-    var equipments = JSON.parse(rawdata);
-  }
-
-  var getAllEquip = () =>{
-    return {"status": 200, "data": equipments};
-  }
-
-var createEquip = (post) =>{
-  let obj  = {
-    id: equipments.length > 0 ? equipments[equipments.length-1].id + 1 : 0, //if blog größer länge 0 mache ID, ansonsten mache 0
-    itemnumber: post.itemnumber,
-    title: post.title,
-    image: post.image, //Später
-    description: post.description,
-    number: Number(post.number),
-    userId: Number(post.userId) //???
-  }
-  equipments.push(obj);
-
-  var data = JSON.stringify(equipments, null, 2);
-  fs.writeFileSync('models/equipments.json', data);
-  return {"status": 201, "data": obj};
-
+  var equipments = [];
+} else {
+  var equipments = JSON.parse(rawdata);
 }
 
-var getIdEquip = (id) =>{
+
+
+//GET:
+var getAllEquip = () => {
+  return { "status": 200, "data": equipments };
+}
+
+//POST:
+var createEquip = (post) => {
+  if (userdata.length > 0){
+  var users = JSON.parse(rawdata);
+  for (let i = 0; i < users.length; i++){
+    if(users[i].id == post.userId){
+      let obj = {
+        id: equipments.length > 0 ? equipments[equipments.length - 1].id + 1 : 0, //if blog größer länge 0 mache ID, ansonsten mache 0
+        itemnumber: post.itemnumber,
+        title: post.title,
+        image: post.image, //Später
+        description: post.description,
+        quantity: Number(post.quantity), //ersteller des Objekts kennt die Anzahl
+        userId: Number(post.userId) //???
+      }
+      equipments.push(obj);
+    
+      var data = JSON.stringify(equipments, null, 2);
+      fs.writeFileSync('models/equipments.json', data);
+      return { "status": 201, "data": obj };
+    }
+  }
+  }
+  return{ "status": 201, "data": "Kein bestehenden Verwalter gefunden"};
+}
+
+//GET ID:
+var getIdEquip = (id) => {
   let obj = equipments.find(element => element.id == id);
-if (obj != null){
-  return {"status": 200, "data": obj};
-}
-else{
-  return {"status": 204, "data": "Equipment not found"};
-}
+  if (obj != null) {
+    return { "status": 200, "data": obj };
+  }
+  else {
+    return { "status": 204, "data": "Equipment not found" };
+  }
 }
 
-var putIdEquip = (id,post) =>{
-  for (let i = 0; i < equipments.length; i++){
-    if(equipments[i].id == id){
-      let newObj  = {
+var changeEquip = (id, post) => {
+  if (userdata.length > 0){
+    var users = JSON.parse(rawdata);
+    for (let i = 0; i < users.length; i++){
+      if(users[i].id == post.userId){
+  for (let i = 0; i < equipments.length; i++) {
+    if (equipments[i].id == id) {
+      let newObj = {
         id: Number(id), //Statt eine gernerierte id, die übergebenen überschrieben
         itemnumber: post.itemnumber,
         title: post.title,
-        image: post.image, 
+        image: post.image,
         description: post.description,
-        number: Number(post.number),
-        userId: Number(post.userId) 
+        quantity: Number(post.quantity),
+        userId: Number(post.userId)
       }
-      equipments[i] = newObj; //ersetzt altes
+      equipments[i] = newObj; //ersetzt altes Objekt
 
-  var data = JSON.stringify(equipments, null, 2);
-  fs.writeFileSync('models/equipments.json', data);
-  return {"status": 201, "data": newObj};
+      var data = JSON.stringify(equipments, null, 2);
+      fs.writeFileSync('models/equipments.json', data);
+      return { "status": 201, "data": newObj };
     }
-  }  return {"status": 404, "data": "No Equipment found"};
- 
+  } return { "status": 404, "data": "No Equipment found" };
+      }//user
+    }//1.for
+}//if
+return{ "status": 201, "data": "Kein bestehenden Verwalter gefunden"};
 }
 
-var    deleteIdEquip  = (id) =>{
+var deleteEquip = (id) => {
   let index = equipments.findIndex(element => element.id == id);
   if (index > -1) {//exestiert ein Eintrag
     equipments.splice(index, 1);
 
     var data = JSON.stringify(equipments, null, 2);
     fs.writeFileSync('models/equipments.json', data);
-  
-    return {"status": 200, "data": equipments}; //gefunden
+
+    return { "status": 200, "data": equipments }; //gefunden
   } else {
-    return {"status": 404, "data": "No Equipment found"}
+    return { "status": 404, "data": "No Equipment found" }
   }
 }
 
 module.exports = {
-    getAllEquip,   
-    createEquip,
-    getIdEquip,       
-    putIdEquip,
-    deleteIdEquip 
-  }
+  getAllEquip,
+  createEquip,
+  getIdEquip,
+  changeEquip,
+  deleteEquip
+}
--- routes/equipment.js
+++ routes/equipment.js
@@ -5,29 +5,29 @@ var router = express.Router();
 var controller = require('../controllers/equipmentController');
 
 router.route('/')
-.get((req,res,next)=>{
-    let response = controller.getAllEquip();
- res.status(response.status).json(response.data);
-})
+    .get((req, res, next) => {
+        let response = controller.getAllEquip();
+        res.status(response.status).json(response.data);
+    })
 
-.post((req,res,next)=>{
-    let response = controller.createEquip(req.body);
-    res.status(response.status).json(response.data);
-})
+    .post((req, res, next) => {
+        let response = controller.createEquip(req.body);
+        res.status(response.status).json(response.data);
+    })
 
 router.route('/:id')
-.get((req,res,next) =>{
-    let response = controller.getIdEquip(req.params.id);    
-    res.status(response.status).json(response.data);
-})
+    .get((req, res, next) => {
+        let response = controller.getIdEquip(req.params.id);
+        res.status(response.status).json(response.data);
+    })
 
-.put((req,res,next) =>{
-    let response = controller.putIdEquip(req.params.id, req.body); 
-    res.status(response.status).json(response.data);   
-})
-.delete((req,res,next) =>{
-    let response = controller.deleteIdEquip(req.params.id);  
-    res.status(response.status).json(response.data);   
-})
+    .put((req, res, next) => {
+        let response = controller.changeEquip(req.params.id, req.body);
+        res.status(response.status).json(response.data);
+    })
+    .delete((req, res, next) => {
+        let response = controller.deleteEquip(req.params.id);
+        res.status(response.status).json(response.data);
+    })
 
 module.exports = router;
