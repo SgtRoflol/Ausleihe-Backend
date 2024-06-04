cdd05fc218cfc4a0a90450a434ec896480d48897
passsswort <theresaboehns@gmail.com>
2024-06-04 14:57:49 +0200

    Borrow in Controller überführt c:

---
+++ controllers/borrowController.js
@@ -0,0 +1,75 @@
+var express = require('express');
+var router = express.Router();
+
+const fs = require("fs");
+const data = fs.readFileSync("models/borrows.json");
+let borrows = JSON.parse(data);
+
+//alle borrow Vorgänge anzeigen lassen
+const getAllBorrows = (req, res, next) => {
+    return borrows;
+}
+
+//borrow Vorgang erstellen
+const createNewBorrow = (req, res, next) => {
+    let begDate = new Date().toISOString().slice(0, 10); //heutiges Datum ohne Uhrzeit
+    let endDate = new Date(Date.now() + 12096e5).toISOString().slice(0, 10); //12096e5 = 2 Wochen in Millisekunden
+
+    let obj = {
+        id: borrows[borrows.length - 1].id + 1,  //AusleihID - Datum enthalten für bessere Verwaltung?
+        userID: Number(req.body["userID"]),   //User der den Artikel ausgeliehen hat
+        begin: begDate,   //beginn Datum
+        end: endDate,            //enddatum zwei Wochen Ausleihzeit
+        powerbankID: Number(req.body["powerbankID"])  //ID der Powerbank
+    }
+    borrows.push(obj);
+
+    const out = JSON.stringify(borrows, null, 2);
+    fs.writeFileSync("models/borrows.json", out);
+    return obj;
+}
+
+//borrow Vorgang an id Stelle anzeigen lassen
+const findID = (req, res, next) => {
+    const index = borrows.findIndex(obj => obj.id == req.params.id);
+    return borrows[index];
+}
+//userID editieren
+const editBorrows = (req, res, next) => {
+    let index = -1
+    index = borrows.findIndex(obj => obj.id == req.params.id);
+    let obj = {
+        id: borrows[index],
+        userID: req.body.userID ? req.body.userID : borrows[index].userID,
+        begin: req.body.begin ? req.body.begin : borrows[index].begin,
+        end: req.body.end ? Number(req.body.end) : Number(borrows[index].end),
+        powerbankID: req.body.powerbankID ? Number(req.body.powerbankID) : Number(borrows[index].powerbankID)
+    }
+    borrows[index] = obj;
+    let data = JSON.stringify(borrows, null, 2);
+    fs.writeFileSync('models/borrows.json', data);
+    if (index == -1) {
+        return { status: 201, out: obj }
+    }
+    else {
+        return { status: 200, out: obj }
+    }
+}
+
+//borrow Vorgang löschen
+const deleteBorrow = (req, res, next) => {
+    const index = borrows.findIndex(obj => obj.id == req.params.id);
+    borrows.splice(index, 1);
+    let data = JSON.stringify(borrows, null, 2);
+    fs.writeFileSync('models/borrows.json', data);
+    return "gelöscht";
+
+}
+
+module.exports = {
+    getAllBorrows,
+    createNewBorrow,
+    findID,
+    editBorrows,
+    deleteBorrow
+}
--- routes/borrows.js
+++ routes/borrows.js
@@ -1,97 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
-
-const fs = require("fs");
-const data = fs.readFileSync("./models/borrows.json");
-let borrows = JSON.parse(data);
-
 //import controller functions
-var controller = require('../controllers/borrowController');
+var borrowController = require('../controllers/borrowController');
 const { stringify } = require('querystring');
 const { request } = require('node:https');
 
 
-//alle borrow Vorgänge anzeigen lassen
-router.get("/", (req, res, next) => {
-    res.status(200).send(borrows);
-});
+router.route("/")
+    //alle borrow Vorgänge anzeigen lassen
+    .get((req, res, next) => {
+        let response = borrowController.getAllBorrows(req);
+        res.status(200).send(response);
+    })
 
-//borrow Vorgang erstellen
-router.post("/", (req, res, next) => {
-    let begDate = new Date().toISOString().slice(0, 10); //heutiges Datum ohne Uhrzeit
-    let endDate = new Date(Date.now() + 12096e5).toISOString().slice(0, 10); //12096e5 = 2 Wochen in Millisekunden
+    //borrow Vorgang erstellen
+    .post((req, res, next) => {
+        let response = borrowController.createNewBorrow(req);
+        res.status(201).send(response);
+    });
 
-    let obj = {
-        id: borrows[borrows.length - 1].id + 1,  //AusleihID - Datum enthalten für bessere Verwaltung?
-        userID: req.body["userID"],   //User der den Artikel ausgeliehen hat
-        begin: begDate,   //beginn Datum
-        end: endDate,            //enddatum zwei Wochen Ausleihzeit
-        powerbankID: req.body["powerbankID"]  //ID der Powerbank
-    }
-    borrows.push(obj);
+router.route("/:id")
+    //borrow Vorgang an id Stelle anzeigen lassen
+    .get((req, res, next) => {
+        let response = borrowController.findID(req);
+        res.status(200).send(response);
+    })
+    //userID editieren
+    .put((req, res, next) => {
+        let response = borrowController.editBorrows(req);
+        res.status(response.status).send(response.out);
+    })
 
-    const out = JSON.stringify(borrows, null, 2);
-    fs.writeFileSync("public/data/borrows.json", out);
-    res.status(200).send(obj);
-});
-
-//borrow Vorgang an id Stelle anzeigen lassen
-router.get("/:id", (req, res, next) => {
-    const index = borrows.findIndex(obj => obj.id == req.params.id);
-    res.status(200).send(borrows[index]);
-});
-
-
-//userID editieren
-router.put("/:id/userID/:input", (req, res, next) => {
-    const index = borrows.findIndex(obj => obj.id == req.params.id);
-    borrows[index].userID = req.params.input;
-    let data = JSON.stringify(borrows, null, 2);
-    fs.writeFileSync('public/data/borrows.json', data);
-    res.status(200).send("userID geändert!");
-
-});
-
-//begin editieren
-router.put("/:id/begin/:input", (req, res, next) => {
-    const index = borrows.findIndex(obj => obj.id == req.params.id);
-    borrows[index].begin = req.params.input;
-    let data = JSON.stringify(borrows, null, 2);
-    fs.writeFileSync('public/data/borrows.json', data);
-    res.status(200).send("begin geändert!");
-
-});
-
-//end editieren
-router.put("/:id/end/:input", (req, res, next) => {
-    const index = borrows.findIndex(obj => obj.id == req.params.id);
-    borrows[index].end = req.params.input;
-    let data = JSON.stringify(borrows, null, 2);
-    fs.writeFileSync('public/data/borrows.json', data);
-    res.status(200).send("end geändert!");
-
-});
-
-//powerbankID editieren
-router.put("/:id/powerbankID/:input", (req, res, next) => {
-    const index = borrows.findIndex(obj => obj.id == req.params.id);
-    borrows[index].powerbankID = req.params.input;
-    let data = JSON.stringify(borrows, null, 2);
-    fs.writeFileSync('public/data/borrows.json', data);
-    res.status(200).send("powerbankID geändert!");
-
-});
-
-//borrow Vorgang löschen
-router.delete("/:id", (req, res, next) => {
-    const index = borrows.findIndex(obj => obj.id == req.params.id);
-    borrows.splice(index, 1);
-    let data = JSON.stringify(borrows, null, 2);
-    fs.writeFileSync('public/data/borrows.json', data);
-    res.status(200).send("gelöscht");
-});
-
+    //borrow Vorgang löschen
+    .delete((req, res, next) => {
+        let response = borrowController.deleteBorrow(req);
+        res.status(200).send(response);
+    });
 
 
 module.exports = router;
